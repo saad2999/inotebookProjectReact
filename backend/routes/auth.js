3f7c792cbde26ea732c51f486b96c74ad0f40e62
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2023-06-26 00:09:00 +0500

    video 46 completed -user validated using express validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,25 @@
 const express= require('express');
 const User= require('../models/User');
+const { body, validationResult } = require('express-validator');
+
 const router=express.Router();
 
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    const user = User(req.body);
-    user.save();
-    res.send("hello world")
+router.post('/',[
+    body('name').isLength({min:3}),
+    
+    body('email').isEmail(),
+    body('password').isLength({min:8})
+],(req,res)=>{
+    const result= validationResult(req);
+    if (!result.isEmpty()) {
+       return res.status(400).json({ errors: result.array() });
+      }
+      User.create({name:req.body.name,
+         email: req.body.email, password: req.body.password
+        }).then((user)=>res.json(user))
+        .catch(errors => res.json({ errors:'email is already existed, please try again'}));
+    
+      
+   
 })
 module.exports = router;
