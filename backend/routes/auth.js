f43a6e5cbb8fb13c383edeaab90085a82571fbd4
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2023-06-26 19:52:16 +0500

    video 49 completed working with jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,9 @@
 const express= require('express');
 const User= require('../models/User');
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET="something$secretishere"
 const router=express.Router();
 
 router.post ('/createuser',[
@@ -24,19 +26,23 @@ router.post ('/createuser',[
         console.log("email already exists")
         return res.status(400).json({ error: "user already exists with this email" });
       }
+      const salt=  await bcrypt.genSalt(10);
+      let secPass= await bcrypt.hash( req.body.password, salt );
      user = await User.create  ({
         name:req.body.name,
-
-         email: req.body.email,
-        password: req.body.password,
+        email: req.body.email,
+        password: secPass,
         });
-        console.log("user created")
+        const data ={
+            id: user.id
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
 
         
         
         // .then((user)=>res.json(user))
         // .catch(errors => res.json({ errors:'email is already existed, please try again'}));
-    res.json(user);
+    res.json({authToken});
 } catch (error) {
     console.log(error.message);
     res.status(500).send("some error occurred")
