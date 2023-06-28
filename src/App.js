8632871fea5012e2ef955a5f97ad4b6d462a3fac
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2023-06-28 20:29:25 +0500

    video 56 completed - navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
+     <Router>
+      <Navbar/>
+      <Routes>
+       
+      <Route exect path="/" element={ <Home/>} />
+      <Route exect path="/about" element={ <About/>} />
+
+      </Routes>
       <h1>my react app</h1>
-    </div>
+      </Router>
+    </>
   );
 }
 
